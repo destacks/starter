50815f95d552ea3f100edb13627954706bddf65c
Alexis Michaltsis <a.michaltsis@gmail.com>
2020-12-21 15:38:34 +0100

    Style pages, add some lorem content

--- t1/v2/pages/posts/first-post.js
+++ t1/v2/pages/posts/first-post.js
@@ -1,14 +1,26 @@
+import Head from "next/head";
 import Link from "next/link";
+
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+
       <h1>First Post</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        nesciunt dolores, amet inventore quaerat harum deserunt earum expedita
+        possimus aperiam.
+      </p>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
